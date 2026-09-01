4f842a366d8b1b35cce1e01fb53fed61cb0f9b9f
crazymt <ytumaotong@gmail.com>
2026-09-01 15:54:21 +0800

    remove ndk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     namespace = "com.example.code_zero"
     compileSdk = 36
 
-    ndkVersion = "27.0.12077973"
+//    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
